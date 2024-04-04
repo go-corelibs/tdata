129fc41aa21a21e34db7bf0efa66feeab78af6d5
Kevin C. Krinke <kevin@krinke.ca>
2024-04-04 14:26:58 -0400

    testdata: migrate to new tdata construct; added TestData type check; added Name method

--- testdata.go
+++ testdata.go
@@ -25,6 +25,8 @@ import (
 	clPath "github.com/go-corelibs/path"
 )
 
+var _ TestData = (*testdata)(nil)
+
 var (
 	// DefaultTestData is the default name of the top-level package directory used
 	// for storing unit test data files, "testdata" by default
@@ -34,40 +36,16 @@ var (
 // TestData is an interface for interacting with a project's top-level
 // unit testing files directory
 type TestData interface {
-	// Path returns the absolute path to this instance's testdata directory
-	Path() (path string)
-	// Join is a convenience wrapper around Path and filepath.Join
-	Join(names ...string) (joined string)
-	// E reports whether the given file exists (as a file or a directory) within
-	// the testdata filesystem
-	E(filename string) (exists bool)
-	// F reads the given file from the testdata filesystem and returns the contents
-	F(filename string) (contents string)
-	// L lists files and directories within the dirname given
-	L(dirname string) (found []string)
-	// LD lists directories within the dirname given
-	LD(dirname string) (found []string)
-	// LF lists files within the dirname given
-	LF(dirname string) (found []string)
-	// LA lists files and directories, recursively
-	LA(dirname string) (found []string)
-	// LAF lists files, recursively
-	LAF(dirname string) (found []string)
-	// LAD lists directories, recursively
-	LAD(dirname string) (found []string)
-	// LH is the same as L except including hidden files
-	LH(dirname string) (found []string)
-	// LAH is the same as LA except including hidden files
-	LAH(dirname string) (found []string)
-	// LAFH is the same as LAF except including hidden files
-	LAFH(dirname string) (found []string)
-	// LADH is the same as LAD except including hidden files
-	LADH(dirname string) (found []string)
+	// Name returns the name of the testdata directory
+	Name() (name string)
+
+	TData
 }
 
 type testdata struct {
+	tdata
+
 	name string
-	path string
 }
 
 // New is a convenience wrapper around NewNamed and the DefaultTestData
@@ -112,74 +90,6 @@ func newTestData(depth int, custom string) (td *testdata) {
 	panic(fmt.Errorf("%w: %s", ErrNotFound, custom))
 }
 
-func (td *testdata) Path() (path string) {
-	return td.path
-}
-
-func (td *testdata) Join(names ...string) (joined string) {
-	return filepath.Join(append([]string{td.path}, names...)...)
-}
-
-func (td *testdata) E(filename string) (exists bool) {
-	exists = clPath.Exists(filepath.Join(td.path, filename))
-	return
-}
-
-func (td *testdata) F(filename string) (contents string) {
-	if data, err := os.ReadFile(filepath.Join(td.path, filename)); err == nil {
-		contents = string(data)
-	}
-	return
-}
-
-func (td *testdata) L(dirname string) (found []string) {
-	found, _ = clPath.List(filepath.Join(td.path, dirname), false)
-	return
-}
-
-func (td *testdata) LD(dirname string) (found []string) {
-	found, _ = clPath.ListDirs(filepath.Join(td.path, dirname), false)
-	return
-}
-
-func (td *testdata) LF(dirname string) (found []string) {
-	found, _ = clPath.ListFiles(filepath.Join(td.path, dirname), false)
-	return
-}
-
-func (td *testdata) LA(dirname string) (found []string) {
-	found = append(found, td.LAD(dirname)...)
-	found = append(found, td.LAF(dirname)...)
-	return
-}
-
-func (td *testdata) LAD(dirname string) (found []string) {
-	found, _ = clPath.ListAllDirs(filepath.Join(td.path, dirname), false)
-	return
-}
-
-func (td *testdata) LAF(dirname string) (found []string) {
-	found, _ = clPath.ListAllFiles(filepath.Join(td.path, dirname), false)
-	return
-}
-
-func (td *testdata) LH(dirname string) (found []string) {
-	found, _ = clPath.List(filepath.Join(td.path, dirname), true)
-	return
-}
-
-func (td *testdata) LAH(dirname string) (found []string) {
-	found = append(found, td.LADH(dirname)...)
-	found = append(found, td.LAFH(dirname)...)
-	return
-}
-
-func (td *testdata) LADH(dirname string) (found []string) {
-	found, _ = clPath.ListAllDirs(filepath.Join(td.path, dirname), true)
-	return
-}
-
-func (td *testdata) LAFH(dirname string) (found []string) {
-	found, _ = clPath.ListAllFiles(filepath.Join(td.path, dirname), true)
-	return
+func (td *testdata) Name() (name string) {
+	return td.name
 }
